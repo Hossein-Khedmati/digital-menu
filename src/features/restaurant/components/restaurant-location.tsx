bc657b598975e745b184c47d0fd290dc4370b199
Hossein Khedmati <khedmati.hossein1382@gmail.com>
2026-07-22 16:43:08 +0330

    feat: add new style and update the landing page fix the upload error of the items update readme

--- src/features/restaurant/components/restaurant-location.tsx
+++ src/features/restaurant/components/restaurant-location.tsx
@@ -9,30 +9,16 @@ type Props = {
 };
 
 export function RestaurantLocation({ lat, lng, name }: Props) {
-  const geoUrl = `geo:${lat},${lng}?q=${lat},${lng}(${encodeURIComponent(name)})`;
-  const fallbackGoogleUrl = `https://www.google.com/maps?q=${lat},${lng}`;
-
-  const handleNavigation = async (e: React.MouseEvent<HTMLButtonElement>) => {
-    if (navigator.share) {
-      e.preventDefault();
-      try {
-        await navigator.share({
-          title: name,
-          text: `مسیریابی به سمت ${name}`,
-          url: fallbackGoogleUrl,
-        });
-      } catch (error) {
-        window.open(fallbackGoogleUrl, "_blank");
-      }
-    } else {
-      window.open(fallbackGoogleUrl, "_blank");
-    }
+  const openGoogleMaps = () => {
+    const query = `${lat},${lng}`;
+    const url = `https://www.google.com/maps?q=${query}`;
+    window.open(url, "_blank");
   };
 
   return (
-    <div className="flex items-center gap-2 ">
-      <Button onClick={handleNavigation} className="max-sm:p-2.5 px-3">
-        <IconMapPin />
+    <div className="flex items-center gap-2">
+      <Button onClick={openGoogleMaps} className="max-sm:p-2.5 px-3">
+        <IconMapPin size={20} />
         <span className="max-sm:hidden text-sm">مسیریابی</span>
       </Button>
     </div>
